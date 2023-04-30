24871e5709289051e726aa963259e6504482b725
Igor Gusakov <igorgusakov199918@gmail.com>
2023-05-01 01:25:03 +0600

    five

--- src/store/reducers/auth/types.ts
+++ src/store/reducers/auth/types.ts
@@ -1,15 +1,38 @@
+import { IUser } from "../../../models/IUser";
+
 export interface AuthState{
     auth: boolean
+    user: IUser;
+    isLoading: boolean;
+    error:string
 }
 
 
 export enum AuthActionEnum {
-    SET_AUTH = 'SET_AUTH'
+    SET_AUTH = 'SET_AUTH',
+    SET_ERROR = 'SET_ERROR',
+    SET_USER = 'SET_USER',
+    SET_LOADING = 'SET_LOADING',
 }
 
 export interface SetAuthAction{
-    type: AuthAction.SET_AUTH,
+    type: AuthActionEnum.SET_AUTH,
     payload: boolean
 }
 
-export type AuthAction = SetAuthAction
+export interface SetErrorAction{
+    type: AuthActionEnum.SET_ERROR,
+    payload: string
+}
+
+export interface SetUserAction{
+    type: AuthActionEnum.SET_USER,
+    payload: IUser
+}
+
+export interface SetLoadingAction{
+    type: AuthActionEnum.SET_LOADING,
+    payload: boolean
+}
+
+export type AuthAction = SetAuthAction | SetErrorAction | SetUserAction | SetLoadingAction
