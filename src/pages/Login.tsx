b7dccc4137bc713ea29255d3b37826e85a3c3bc3
Igor Gusakov <igorgusakov199918@gmail.com>
2023-05-09 20:37:56 +0600

    end

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,8 +6,20 @@ const Login: FC = () => {
     return (
         <>
             <Layout>
+            <Row justify='center' align='middle' className='h0'>
+            <div style={{fontSize:'18px', color:'red'}}> For test      "username": "user",
+                        "password": "123"
+
+                        "username": "admin",
+                        "password": "123"
+
+                        "username": "Igor",
+                        "password": "123"
+                        </div>
+                </Row>
                 <Row justify='center' align='middle' className='h100'>
-                 <LoginForm></LoginForm>
+                    
+                    <LoginForm></LoginForm>
                 </Row>
             </Layout>
         </>
